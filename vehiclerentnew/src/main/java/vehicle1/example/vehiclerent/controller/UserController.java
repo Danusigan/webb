b689c64661f96b5c3da40c7d30a035afa79a2243
Danusigan <j.danusigan02@gmail.com>
2025-09-07 00:32:40 +0530

    Update UserController.java

--- vehiclerentnew/src/main/java/vehicle1/example/vehiclerent/controller/UserController.java
+++ vehiclerentnew/src/main/java/vehicle1/example/vehiclerent/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:3001")
 @RequestMapping("/api/users")
 public class UserController {
 
