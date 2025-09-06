acf128973de9577520f47d2f9872907c535a0aba
Danusigan <j.danusigan02@gmail.com>
2025-09-06 12:09:26 +0530

    Update and rename VehiclerentApplication.java to vehiclerentApplication.java

--- vehiclerentnew/src/main/java/vehicle1/example/vehiclerent/vehiclerentApplication.java
+++ vehiclerentnew/src/main/java/vehicle1/example/vehiclerent/vehiclerentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VehiclerentApplication {
+public class vehiclerentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VehiclerentApplication.class, args);
+		SpringApplication.run(vehiclerentApplication.class, args);
 	}
 
 }
