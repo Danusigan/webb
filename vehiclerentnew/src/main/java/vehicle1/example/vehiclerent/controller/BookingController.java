b47a2ce42f587d5d05aaa8c01ea64392d24a846c
Danusigan <j.danusigan02@gmail.com>
2025-09-07 00:55:33 +0530

    Update BookingController.java

--- vehiclerentnew/src/main/java/vehicle1/example/vehiclerent/controller/BookingController.java
+++ vehiclerentnew/src/main/java/vehicle1/example/vehiclerent/controller/BookingController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:3001")
 @RequestMapping("/api/bookings")
 public class BookingController {
 
